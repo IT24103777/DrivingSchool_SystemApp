c5f3444e1466bd38dbdd41bba379f6b576826a3a
sandew0412 <sandew.mewanmin@gmail.com>
2025-05-20 17:40:45 +0530

    uploading the StudentModel.java file

---
+++ src/main/java/com/Driving_School_System/modules/student_management/StudentModel.java
@@ -0,0 +1,68 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package com.Driving_School_System.modules.student_management;
+
+import com.Driving_School_System.common.BaseEntity;
+
+public class StudentModel extends BaseEntity {
+    private String name;
+    private String address;
+    private String studentNumber;
+    private String contactNumber;
+    private String email;
+    private int age;
+
+    public StudentModel() {
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getStudentNumber() {
+        return this.studentNumber;
+    }
+
+    public void setStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public String getContactNumber() {
+        return this.contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
